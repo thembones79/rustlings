778756cf5346e15f39418458b6f0997b92758091
thembones79 <michal.szumnarski@gmail.com>
2023-04-02 00:22:19 +0200

    ex91

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,24 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        match s.split_once(',') {
+            Some((name, age)) => {
+                if name.len() == 0 {
+                    Person::default()
+                } else {
+                    match age.parse::<usize>() {
+                        Ok(a) => Person {
+                            name: String::from(name),
+                            age: a,
+                        },
+                        Err(_) => Person::default(),
+                    }
+                }
+            }
+            _ => Person::default(),
+        }
     }
 }
 
